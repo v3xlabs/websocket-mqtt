5ee95a082247bd418b155a576f2e80044cb70534
Luc <luc@lucemans.nl>
2026-01-25 22:04:29 +0100

    base

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-// index
+export type ConnectionOptions = {};
+export type MqttClient = {};
+
+export const connect = async (url: string, options: ConnectionOptions): Promise<MqttClient> => {
+
+    return {};
+};
---
+++ tests/index.bench.ts
@@ -0,0 +1,19 @@
+import { test, expect, bench } from 'vitest';
+import { connect as connectMqtt } from 'mqtt';
+
+bench('mqtt connection', () => {
+    const client = connectMqtt('mqtt://test.mosquitto.org', {});
+
+    client.on('connect', async () => {
+        await client.subscribeAsync('mqtt/test');
+
+        await client.publishAsync('mqtt/test', 'Hello, world!');
+    });
+
+    client.on('message', (topic, message) => {
+        console.log(topic, message.toString());
+        client.end();
+        // test complete
+        expect(true).toBe(true);
+    });
+}, { time: 1000, iterations: 5 });
